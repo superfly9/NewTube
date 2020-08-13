2cdecc4d1a122da8613b44d6c6acc6da86982803
unknown <superfly09@daum.net>
2020-08-13 15:05:06 +0900

    Updates

---
+++ precache-manifest.335ff01ca42cc558f7d59010e0afb04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64b7ad5b367dbbc8b0169699a5b052c",
+    "url": "/newtube/index.html"
+  },
+  {
+    "revision": "30f8c590821b484c4d75",
+    "url": "/newtube/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "d601d8db02a04e01c2da",
+    "url": "/newtube/static/css/main.5d39222c.chunk.css"
+  },
+  {
+    "revision": "30f8c590821b484c4d75",
+    "url": "/newtube/static/js/2.f7b16ed4.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/newtube/static/js/2.f7b16ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d601d8db02a04e01c2da",
+    "url": "/newtube/static/js/main.585981fa.chunk.js"
+  },
+  {
+    "revision": "6565de738fce47424f63",
+    "url": "/newtube/static/js/runtime-main.f1489b67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.github.io/newtube/precache-manifest.b55c3edfb73a44215a2d029d343f2925.js"
+  "/newtube/precache-manifest.335ff01ca42cc558f7d59010e0afb04a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.github.io/newtube/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newtube/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
