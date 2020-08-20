31d121483cda94b23bc3f306bf8d921cd570e649
unknown <superfly09@daum.net>
2020-08-20 13:52:07 +0900

    fix: 로그인 시에만 구독페이지 이동가능하게 함

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Menu } from 'antd';
+import { useSelector } from 'react-redux';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 function LeftMenu(props) {
+  const user = useSelector(state=>state.user);
+  const {userData}=user;
+  let userId = userData ? userData._id : undefined;
   return (
     <Menu mode={props.mode}>
-    <Menu.Item key="subscribe">
-      <a href="/subscribe">구독 리스트</a>
-    </Menu.Item>
+      {userId ?
+      <Menu.Item key="subscribe">
+        <a href="/subscribe">구독 리스트</a>
+      </Menu.Item>
+      :''
+      }
   </Menu>
   )
 }
